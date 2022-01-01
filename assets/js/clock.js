78ba8b2968410399354b223a54d1bb2670e1c223
Mohamed <93315220+T-W-mohammed@users.noreply.github.com>
2022-01-01 20:47:06 +0200

    Add files via upload

---
+++ assets/js/clock.js
@@ -0,0 +1,83 @@
+/*==================== CLOCK ====================*/
+const hour = document.getElementById('clock-hour'),
+      minutes = document.getElementById('clock-minutes'),
+      seconds = document.getElementById('clock-seconds')
+
+const clock = () =>{
+    let date = new Date()
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6
+        
+    // We add a rotation to the elements
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    minutes.style.transform = `rotateZ(${mm}deg)`
+    seconds.style.transform = `rotateZ(${ss}deg)`
+}
+setInterval(clock, 1000) // 1000 = 1s
+
+/*==================== CLOCK & DATE TEXT ====================*/
+const textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textSeconds = document.getElementById('text-seconds'),
+      textAmPm = document.getElementById('text-ampm'),
+    //   dateWeek = document.getElementById('date-day-week'),
+      dateDay = document.getElementById('date-day'),
+      dateMonth = document.getElementById('date-month'),
+      dateYear = document.getElementById('date-year')
+
+const clockText = () =>{
+    let date = new Date()
+
+    let hh = date.getHours(),
+        ampm,
+        mm = date.getMinutes(),
+        ss = date.getSeconds(),
+        day = date.getDate(),
+        // dayweek = date.getDay(),
+        month = date.getMonth(),
+        year = date.getFullYear()
+
+    // We change the hours from 24 to 12 hours and establish whether it is AM or PM
+    if(hh >= 12){
+        hh = hh - 12
+        ampm = 'PM'
+    }else{
+        ampm = 'AM'
+    }
+
+    // We detect when it's 0 AM and transform to 12 AM
+    if(hh == 0){hh = 12}
+
+    // Show a zero before hours
+    if(hh < 10){hh = `0${hh}`}
+
+    // Show time
+    textHour.innerHTML = `${hh}:`
+    if(ss < 10){ss = `0${ss}`}
+    textSeconds.innerHTML = `:${ss}`
+
+    // Show a zero before the minutes
+    if(mm < 10){mm = `0${mm}`}
+    
+    // Show minutes
+    textMinutes.innerHTML = mm
+
+    // Show am or pm
+    textAmPm.innerHTML = ampm
+
+    // If you want to show the name of the day of the week
+    // let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+
+    // We get the months of the year and show it
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    // We show the day, the month and the year
+    dateDay.innerHTML = day
+    // dateWeek.innerHTML = `${week[dayweek]}`
+    dateMonth.innerHTML = `${months[month]},`
+    dateYear.innerHTML = year
+}
+setInterval(clockText, 1000) // 1000 = 1s
+
